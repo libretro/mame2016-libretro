d716f53fa67e56f163b664dab15d0118a8ea9e5d
not6 <r-type@users.noreply.github.com>
2016-02-28 18:27:39 +0100

    fix (timeout == OSD_EVENT_WAIT_INFINITE)  twice

--- src/osd/modules/sync/sync_retro.c
+++ src/osd/modules/sync/sync_retro.c
@@ -366,9 +366,6 @@ int osd_event_wait(osd_event *event, osd_ticks_t timeout)
 	if (timeout == OSD_EVENT_WAIT_INFINITE)
 			timeout = osd_ticks_per_second() * (osd_ticks_t)10000;
 
-	if (timeout == OSD_EVENT_WAIT_INFINITE)
-			timeout = osd_ticks_per_second() * (osd_ticks_t)10000;
-
 	pthread_mutex_lock(&event->mutex);
 	if (!timeout)
 	{
