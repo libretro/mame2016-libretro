b7cb663434d298f052bacb67d2275f027ba44be4
twinaphex <libretro@gmail.com>
2016-05-26 05:28:39 +0200

    Remove unnecessary files

--- src/osd/retro/retromain.cpp
+++ src/osd/retro/retromain.cpp
@@ -10,9 +10,10 @@
 #include "drivenum.h"
 
 #include "libretro.h"
-#include "retroosd.h"
+#include "options.h"
+#include "osdepend.h"
+#include "modules/lib/osdobj_common.h"
 
-//#include "clifront.h"
 #include "modules/lib/osdobj_common.h"
 #include "modules/lib/osdlib.h"
 #include "modules/osdmodule.h"
@@ -25,6 +26,29 @@ char slash = '\\';
 #else
 char slash = '/';
 #endif
+
+//============================================================
+//  TYPE DEFINITIONS
+//============================================================
+
+class retro_osd_interface : public osd_common_t
+{
+public:
+	// construction/destruction
+	retro_osd_interface(osd_options &options);
+	virtual ~retro_osd_interface();
+
+	// general overridables
+	virtual void init(running_machine &machine);
+	virtual void update(bool skip_redraw);
+
+	// input overridables
+//	virtual void customize_input_type_list(input_type_desc *typelist);
+	virtual void customize_input_type_list(simple_list<input_type_entry> &typelist);
+
+private:
+	virtual void osd_exit();
+};
 
 /* Args for experimental_commandline */
 static char ARGUV[32][1024];
