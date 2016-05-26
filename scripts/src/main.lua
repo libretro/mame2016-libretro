3abf124ba014f17efc197e6e22c95a92333d9307
twinaphex <libretro@gmail.com>
2016-05-26 20:32:41 +0200

    Add back retroprefix.h

--- scripts/src/main.lua
+++ scripts/src/main.lua
@@ -327,6 +327,10 @@ end
    -- RETRO HACK
 	if _OPTIONS["osd"]=="retro" then
 
+      forcedincludes {
+			MAME_DIR .. "src/osd/retro/retroprefix.h"
+		}
+
 		includedirs {
 			MAME_DIR .. "src/emu",
 			MAME_DIR .. "src/osd",
