5c13a2ffffb853034bc2c4c57d84d69187902040
WizzardSK <wizzardsk@gmail.com>
2026-09-06 21:05:49 +0200

    3rdparty/lua: os.execute cannot call system() on iOS or tvOS
      loslib.c:132:14: error: 'system' is unavailable: not available on iOS
    
    Guarded the same way modern MAME's bundled lua guards it.

--- 3rdparty/lua/src/loslib.c
+++ 3rdparty/lua/src/loslib.c
@@ -16,6 +16,10 @@
 #include <string.h>
 #include <time.h>
 
+#ifdef __APPLE__
+    #include "TargetConditionals.h"
+#endif
+
 #include "lua.h"
 
 #include "lauxlib.h"
@@ -128,6 +132,11 @@ static time_t l_checktime (lua_State *L, int arg) {
 
 
 static int os_execute (lua_State *L) {
+#if defined(__APPLE__) && (TARGET_OS_IOS || TARGET_OS_TV)
+  /* system() is unavailable on iOS and tvOS */
+  lua_pushboolean(L, 0);
+  return 1;
+#else
   const char *cmd = luaL_optstring(L, 1, NULL);
   int stat = system(cmd);
   if (cmd != NULL)
@@ -136,6 +145,7 @@ static int os_execute (lua_State *L) {
     lua_pushboolean(L, stat);  /* true if there is a shell */
     return 1;
   }
+#endif
 }
 
 
